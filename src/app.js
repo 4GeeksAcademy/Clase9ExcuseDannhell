a47452c7eecf715557229b51641dada458bc6ab8
Dannhell <161614077+Dannhell@users.noreply.github.com>
2024-06-03 17:49:43 +0000

    Primer

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["Mi jefe", "Mi mascota", "Mi pareja", "Un  ladron"];
+  let accion = [" se  llevo ", " ha cambiado", " ha pintado", " ha  dañado"];
+  let que = [" mi coche ", " mis llaves", " mis vacaciones", " la  laptop"];
+  let cuando = [
+    " en la noche",
+    " durante mis vacaciones ",
+    " al atardecer ",
+    " a la hora de la siesta "
+  ];
+  let QuienNuevo = Math.floor(Math.random() * quien.length);
+  let AccionNuevo = Math.floor(Math.random() * accion.length);
+  let Quenuevo = Math.floor(Math.random() * que.length);
+  let CuandoNuevo = Math.floor(Math.random() * cuando.length);
+  let excusaNueva =
+    quien[QuienNuevo] +
+    accion[AccionNuevo] +
+    que[Quenuevo] +
+    cuando[CuandoNuevo];
+  console.log(excusaNueva);
+  document.getElementById("excusa").innerHTML = excusaNueva;
 };
